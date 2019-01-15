f602b162db93f78824aa3d269f1fce5b678bfdd0
sumedha <sumedhanarayanan@gmail.com>
2019-01-15 01:56:01 +0000

    Linked the downloadImageByURL function to the callback function. Running the file saves all the contributor pictures in folder avatar

--- download_avatars.js
+++ download_avatars.js
@@ -3,29 +3,21 @@ var authToken = require('./secrets.js');
 var fs = require('fs');
 
 //This function gets the contributors for a given repository and parses an object to the call back function
-// function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, repoName, cb) {
 
-//     var options = {
-//         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-//         headers: {
-//             'User-Agent': 'sumedhan',
-//             'Authorization': 'token ' + authToken.GITHUB_TOKEN
-//         }
-//     }
-//     request(options, function(err, res, body) {
+    var options = {
+        url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+        headers: {
+            'User-Agent': 'sumedhan',
+            'Authorization': 'token ' + authToken.GITHUB_TOKEN
+        }
+    }
+    request(options, function(err, res, body) {
         
-//         var content = JSON.parse(body);
-//         cb(err, content);
-//         });
-// }
-
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//     //Call back function declaration. The callback functions access the resuly object to access each contributor's avatar URL
-//     console.log("Errors:", err);
-//     result.forEach(function(contributor){
-//         console.log(contributor['avatar_url']);
-//     })
-// });
+        var content = JSON.parse(body);
+        cb(err, content);
+        });
+}
 
 function downloadImageByURL(url,filepath) {
     request.get(url)
@@ -34,4 +26,15 @@ function downloadImageByURL(url,filepath) {
     })
     .pipe(fs.createWriteStream(filepath));
 }
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466","./test/kv.jpg");
+
+
+getRepoContributors('jquery', 'jquery', function(err, result) {
+    //Call back function declaration. The callback functions access the resuly object to access each contributor's avatar URL
+    console.log('Errors:', err);
+    result.forEach(function(contributor){
+        var url = contributor['avatar_url'];
+        var filepath = 'avatars/' + contributor.login + '.jpg';
+        downloadImageByURL(url, filepath);
+    })
+});
+
